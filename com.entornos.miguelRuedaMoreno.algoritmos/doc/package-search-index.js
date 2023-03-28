1d2c65a1ab47667de5681aee8abe2af8eedd331c
Miguel Angel Rueda Moreno <migue.rueda.90@gmail.com>
2023-03-28 18:29:04 +0200

    Quinto commit

---
+++ com.entornos.miguelRuedaMoreno.algoritmos/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.entornos.miguelRuedaMoreno.algoritmos"}];updateSearchResults();
---
+++ com.entornos.miguelRuedaMoreno.algoritmos/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"com.entornos.miguelRuedaMoreno.algoritmos","l":"Algoritmos"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"}];updateSearchResults();
